917a4ade134ae5db9c32f150f4ae25d25f3000ac
BKleinberg <bennettkleinberg@gmail.com>
2018-10-18 17:19:55 +0200

    db integration

--- js/backend/data_collector.js
+++ js/backend/data_collector.js
@@ -1,18 +1,17 @@
 function collect_php(var_data) {
-  console.log('will trigger php server call');
-
-  // $.ajax({
-  //   type: "POST",
-  //   url: "../php/collect.php",
-  //   data: {
-  //     data_pre: var_data
-  //   },
-  //   error: function(data) {
-  //     // alert(data);
-  //   },
-  //   success: function(data) {
-  //     // console.log(data);
-  //     console.log("intermittent data submitted");
-  //   },
-  // });
+  console.log('triggered php server call');
+  $.ajax({
+    type: "POST",
+    url: "../php/collect.php",
+    data: {
+      data_pre: var_data
+    },
+    error: function(data) {
+      // alert(data);
+    },
+    success: function(data) {
+      // console.log(data);
+      console.log("data submitted");
+    },
+  });
 }
